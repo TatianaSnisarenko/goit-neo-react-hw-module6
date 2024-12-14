a0a4d91c3bbd8b6d4506d3af462a76bc1f923a91
Tatiana Snisarenko <tatiana.snisarenko.p@gmail.com>
2024-12-14 19:01:35 +0200

    Add loader

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,14 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./redux/store";
+import Loader from "./components/Loader/Loader";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+      <PersistGate loading={<Loader />} persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>
